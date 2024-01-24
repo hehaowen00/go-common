3bae30c8e4a1ffe8c1dd7b8eac19ea6287a3c140
hehaowen00 <edmundhe01@gmail.com>
2024-01-25 00:13:17 +1000

    add array map tests

---
+++ arraymap_test.go
@@ -0,0 +1,57 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestArrayMap1(t *testing.T) {
+	table := NewArrayMap[int, string]()
+
+	for i := 0; i < 1000; i++ {
+		table.Set(i, fmt.Sprintf("%d", i))
+	}
+
+	for i := 0; i < 1000; i++ {
+		v, ok := table.Get(i)
+		if ok {
+			if v != fmt.Sprintf("%d", i) {
+				t.Fail()
+			}
+		} else {
+			t.Fail()
+		}
+	}
+}
+
+func BenchmarkArrayMap1(b *testing.B) {
+	b.StopTimer()
+
+	table := NewArrayMap[int, string]()
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		table.Set(i, fmt.Sprintf("%d", i))
+	}
+
+	for i := 0; i < b.N; i++ {
+		table.Get(i)
+	}
+}
+
+func BenchmarkMap1(b *testing.B) {
+	b.StopTimer()
+
+	table := map[int]string{}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		table[i] = fmt.Sprintf("%d", i)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = table[i]
+	}
+}
